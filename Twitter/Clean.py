99233eb5ceece28b9956e63d7f9c1c24a8cdcc4d
chaukap <chaukap@LAPTOP-9TN52VGH.localdomain>
2022-02-27 09:40:34 -0700

    Completed graphic for Facebook, Google, and Twitter

--- Twitter/Clean.py
+++ Twitter/Clean.py
@@ -5,12 +5,12 @@ twitter = twitter[twitter.Country == "United States"][
     ["Time period start", "Combined requests", "Combined accounts specified", "Country"]
 ].dropna()
 
-twitter["half"] = ["Jul-Dec" if "07" in t else "Jan-Jun" for t in twitter["Time period start"]]
+twitter["half"] = [2 if "07" in t else 1 for t in twitter["Time period start"]]
 twitter["year"] = [t[0:4] for t in twitter["Time period start"]]
 
 twitter.columns = ['Time period start', "total_requests", "total_requests_accounts", "country", 'half', 'year']
 
-twitter["time"] = twitter.apply(lambda y: y.half + " " + y.year, axis=1)
+twitter["time"] = twitter.apply(lambda y: ("Jan-Jun" if y.half == 1 else "Jul-Dec") + " " + y.year, axis=1)
 twitter["accounts_per_request"] = twitter.total_requests_accounts / twitter.total_requests.map(lambda t: int(t))
 twitter["site"] = "Twitter"
 
@@ -19,4 +19,4 @@ twitter = twitter[twitter.time != "Jan-Jun 2013"]
 
 twitter = twitter.sort_values(["year", "half"], ascending=True)
 
-twitter.to_csv("../Clean_Data/Twitter.csv")
+twitter.to_csv("../Clean_Data/Twitter.csv", index=False)
